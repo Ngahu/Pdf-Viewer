d6c4337c90b7345987b95d8364764742800d48ca
ngahu nj <joe@made.ke>
2021-08-11 07:04:30 +0300

    Custom PDF interface using pdf.js.

---
+++ js/main.js
@@ -0,0 +1,93 @@
+const url ='../docs/django.pmdf'
+
+let pdfDoc = null,
+    pageNum = 1,
+    pageIsRendering = false,
+    pageNumIsPending = null;
+
+
+const scale = 1.5,
+    canvas = document.querySelector('#pdf-render'),
+    ctx = canvas.getContext('2d');
+
+// Render the page
+const renderPage = num => {
+    pageIsRendering = true;
+
+    // Get the page
+    pdfDoc.getPage(pageNum).then(page =>{
+        // Set scal
+        const viewport = page.getViewport({ scale });
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+
+        const renderContext = {
+            canvasContext: ctx,
+            viewport
+        }
+        page.render(renderContext).promise.then(() => {
+            pageIsRendering = false;
+
+            if (pageNumIsPending !== null){
+                renderPage(pageNumIsPending);
+                pageNumIsPending = null
+            }
+        });
+
+        // Output current page
+        document.querySelector('#page-num').textContent = num;
+    })
+
+}
+
+// Check for pages rendering
+
+const queueRenderPage = num => {
+    if(pageIsRendering) {
+        pageNumIsPending = num;
+    }else{
+        renderPage(num);
+    }
+}
+
+// Show next page
+
+const showNextPage = () => {
+    if (pageNum >= pdfDoc.numPages) {
+        return;
+    }
+    pageNum++;
+    queueRenderPage(pageNum);
+}
+
+// Show previous page
+
+const showPrevPage = () => {
+    if(pageNum <= 1){
+        return;
+    }
+    pageNum--;
+    queueRenderPage(pageNum);
+}
+
+// Get document
+pdfjsLib.getDocument(url).promise.then(pdfDoc_ => {
+    pdfDoc = pdfDoc_;
+    
+    document.querySelector('#page-count').textContent = pdfDoc.numPages;
+
+    renderPage(pageNum)
+})
+.catch(err => {
+    // Display error
+    const div = document.createElement('div');
+    div.className = 'error'
+    div.appendChild(document.createTextNode(err.message));
+    document.querySelector('body').insertBefore(div), canvas;
+    //Remove top bar
+    document.querySelector('.top-bar').style.display='none';
+});
+
+// button events
+document.querySelector('#prev-page').addEventListener('click', showPrevPage);
+document.querySelector('#next-page').addEventListener('click', showNextPage);
